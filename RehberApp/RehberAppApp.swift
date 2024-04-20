a451bdbd215268b69bd53bddcf26ab3348eecc4e
Selil <selilmcp@gmail.com>
2024-04-20 11:14:17 +0100

    create project

---
+++ RehberApp/RehberAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RehberAppApp.swift
+//  RehberApp
+//
+//  Created by Selil on 20/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct RehberAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            LoginView()
+        }
+    }
+}
---
+++ RehberApp/View/LoginView.swift
@@ -0,0 +1,21 @@
+//
+//  LoginView.swift
+//  RehberApp
+//
+//  Created by Selil on 20/04/2024.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        VStack {
+            Text("test")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    LoginView()
+}
