c4bc75d1cc55fbbdb17cff5efd1d0ebe437829f7
Selil <selilmcp@gmail.com>
2024-04-20 12:36:03 +0100

    login view model create with login service

---
+++ RehberApp/ViewModel/LoginViewModel.swift
@@ -0,0 +1,30 @@
+//
+//  LoginViewModel.swift
+//  RehberApp
+//
+//  Created by Selil on 20/04/2024.
+//
+
+import Foundation
+
+class LoginViewModel{
+    
+    private var loginDetails:LoginDetailModel?
+    private let service: LoginService
+    
+    init (service: LoginService) {
+        self.service = service
+    }
+    
+    func fetchDetails(userName:String,
+                      response:@escaping (NetworkError?) -> ()){
+        service.getLoginDetails(userName: userName) { [weak self]  details, error in
+            if let loginDetails = details{
+                self?.loginDetails = loginDetails
+                response(nil)
+            }else if let error = error{
+                response(error)
+            }
+        }
+    }
+}
